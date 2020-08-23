b06db770cffffb13c77b63860abb1636cf73f84c
Joshua Mangi <joshuamangi@gmail.com>
2020-08-23 13:49:14 +0300

    Updates

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const URL = process.env.URL;
 (function() {
   'use strict';
   window.addEventListener('load', function() {
@@ -18,6 +17,7 @@ const URL = process.env.URL;
 })();
 //Process Form data
 function FormProcess(){
+  let url = " https://petrolappapi.herokuapp.com/petroldetails";
   var Mileage = document.getElementById('Mileage').value;
   var PpL = document.getElementById('PpL').value;
   var Amount = document.getElementById('Amount').value;
@@ -26,17 +26,42 @@ function FormProcess(){
   var LicensePlate = document.getElementById('LicensePlate').value;
   var AdditionalInfo = document.getElementById('AdditionalInfo').value;
   //Creating XHR object
-  let xhr = new XMLHttpRequest();
-  xhr.open("POST",URL,true);
+  var xhr = new XMLHttpRequest();
+  if (!('withCredentials' in xhr)) {
+    alert('Browser does not support CORS.');
+    return;
+  }
+  xhr.open("POST",url,false);
+  xhr.onerror = function() {
+    alert('There was an error.');
+  };
   xhr.setRequestHeader('Content-Type','application/json');
-  // Create a state change callback 
-  xhr.onreadystatechange = function () { 
-    if (xhr.readyState === 4 && xhr.status === 200) { 
-        // Print received data from server 
-        result.innerHTML = this.responseText; 
-    } 
-  }; 
-  var petroldata =JSON.stringify({"Mileage":Mileage,"PpL":PpL,"Amount":Amount,"Capacity":Capacity,"PetrolStation":PetrolStation,"LicensePlate":LicensePlate,"AdditionalInfo":AdditionalInfo});
-  xhr.send(petroldata);
+  xhr.send(JSON.stringify({"Mileage":Mileage,"PpL":PpL,"Amount":Amount,"Capacity":Capacity,"PetrolStation":PetrolStation,"LicensePlate":LicensePlate,"AdditionalInfo":AdditionalInfo}));
+  xhr.onload = () =>{
+    alert(xhr.responseText);
+  };
+  xhr.onreadystatechange =() => {//Call a function when the state changes.
+    if(xhr.status == 204) {
+      alert(`Error ${xhr.status}: ${xhr.statusText}`); // e.g. 404: Not Found
+    } else { // show the result
+      alert(`Done, got ${xhr.response.length} bytes`); // response is the server
+    }
+  }
+  /*xhr.onload = function(){
+    if (xhr.status != 204) { // analyze HTTP status of the response
+      alert(`Error ${xhr.status}: ${xhr.statusText}`); // e.g. 404: Not Found
+    } else { // show the result
+      alert(`Done, got ${xhr.response.length} bytes`); // response is the server
+    }
+  }
+  xhr.onprogress = function(event) {
+    if (event.lengthComputable) {
+      alert(`Received ${event.loaded} of ${event.total} bytes`);
+    } else {
+      alert(`Received ${event.loaded} bytes`); // no Content-Length
+    }
+  
+  };
+  xhr.send(petroldata);*/
   location.reload();
 }
--- viewpetroldetails.js
+++ viewpetroldetails.js
@@ -1,11 +1,10 @@
-const URL = process.env.URL;
 //Delete petrol details
 function deletePetrolDetails(Id){
     var delpetroldetail = confirm("Are you sure you want to delete?");
     if(delpetroldetail == true){
       const xhr = new XMLHttpRequest();
-      let URL = URL + `/${Id}`
-      xhr.open("DELETE",URL, false);
+      let url = ` https://petrolappapi.herokuapp.com/petroldetails/${Id}`
+      xhr.open("DELETE",url, false);
       xhr.send();
       location.reload();
     } else{
@@ -15,8 +14,8 @@ function deletePetrolDetails(Id){
 //Update petrol details
 function setEditModal(Id){
     const xhr = new XMLHttpRequest();
-    let URL = URL + `/${Id}`
-    xhr.open("GET",URL, false);
+    let url = ` https://petrolappapi.herokuapp.com/petroldetails/${Id}`
+    xhr.open("GET",url, false);
     xhr.send();
     const petroldetail = JSON.parse(xhr.responseText);
 
@@ -39,69 +38,76 @@ function setEditModal(Id){
     document.getElementById('LicensePlate').value = LicensePlate; 
     document.getElementById('AdditionalInfo').value = AdditionalInfo;
     //Set the action on the form
-    //document.getElementById('editpetroldetailsform').action = URL;
+    //document.getElementById('editpetroldetailsform').action = url;
 }
-
 //Put Petrol Details after update
 function PutPetrolDetails(){
-    //alert("This has worked");
-    var Id = document.getElementById('Id').value;
-    var Mileage = document.getElementById('Mileage').value;
-    var Date = document.getElementById('Date').value;
-    var PpL = document.getElementById('PpL').value;
-    var Amount = document.getElementById('Amount').value;
-    var Capacity = document.getElementById('Capacity').value;
-    var PetrolStation = document.getElementById('PetrolStation').value;
-    var LicensePlate = document.getElementById('LicensePlate').value; 
-    var AdditionalInfo = document.getElementById('AdditionalInfo').value;
-    let xhr = new XMLHttpRequest();
-    let URL = URL + `/${Id}`;
-    xhr.open("PUT",URL,true);
-    xhr.setRequestHeader('Content-type','application/json; charset=utf-8');
-    xhr.onreadystatechange = function () { 
-      if (xhr.readyState === 4 && xhr.status === 200) { 
-          // Print received data from server 
-          result.innerHTML = this.responseText; 
-      } else {
-        console.error(users);
-      }
-    }; 
-    var petroldata = JSON.stringify({"Mileage":Mileage,"Date":Date,"PpL":PpL,"Amount":Amount,"Capacity":Capacity,"PetrolStation":PetrolStation,"LicensePlate":LicensePlate,"AdditionalInfo":AdditionalInfo});
-    xhr.send(petroldata);
-    location.reload();
+  //alert("This has worked");
+  var Id = document.getElementById('Id').value;
+  var Mileage = document.getElementById('Mileage').value;
+  var Date = document.getElementById('Date').value;
+  var PpL = document.getElementById('PpL').value;
+  var Amount = document.getElementById('Amount').value;
+  var Capacity = document.getElementById('Capacity').value;
+  var PetrolStation = document.getElementById('PetrolStation').value;
+  var LicensePlate = document.getElementById('LicensePlate').value; 
+  var AdditionalInfo = document.getElementById('AdditionalInfo').value;
+  var xhr = new XMLHttpRequest();
+  if (!('withCredentials' in xhr)) {
+    alert('Browser does not support CORS.');
+    return;
+  };
+  let url = ` https://petrolappapi.herokuapp.com/petroldetails/${Id}`;
+  xhr.open("PUT",url,false);
+  xhr.onerror = function() {
+    alert('There was an error.');
+  };
+  xhr.setRequestHeader('Content-Type','application/json'); 
+  xhr.send(JSON.stringify({"Mileage":Mileage,"Date":Date,"PpL":PpL,"Amount":Amount,"Capacity":Capacity,"PetrolStation":PetrolStation,"LicensePlate":LicensePlate,"AdditionalInfo":AdditionalInfo}));
+  xhr.onload = () =>{
+    alert(xhr.responseText);
+  };
+  xhr.onreadystatechange =() => {//Call a function when the state changes.
+    if(xhr.status == 204) {
+      alert(`Error ${xhr.status}: ${xhr.statusText}`); // e.g. 404: Not Found
+    } else { // show the result
+      alert(`Done, got ${xhr.response.length} bytes`); // response is the server
     }
-    //Load Petrol details in view page
-    function LoadPetrolDetails(){
-    const xhr = new XMLHttpRequest();
-    let URL = "URL";
-    xhr.open("GET",URL,false);
-    xhr.send();
-    const petroldetails = JSON.parse(xhr.responseText);
-    for(let petroldetail of petroldetails){
-      const petrolview = `
-        <div class="col-4">
-        <div class="card text-center text-white bg-dark border-dark mb-3">
-            <div class="card-body">
-                <h5 class="card-title">${petroldetail.Date}</h5>
-                <h6 class="card-subtitle mb-2">${petroldetail.Id}</h6>
-                <h6 class="card-subtitle mb-2">${petroldetail.Mileage}</h6>
-                <div>Amount: ${petroldetail.Amount}</div>
-                <div>Price Per Liter: ${petroldetail.PpL}</div>
-                <div>Capacity: ${petroldetail.Capacity}</div>
-                <div>Petrol Station: ${petroldetail.PetrolStation}</div>
-                <div>License Plate: ${petroldetail.LicensePlate}</div>
-                <div>Additional Info: ${petroldetail.AdditionalInfo}</div>
-                <hr>
-                <button type="button" class="btn btn-danger" onclick="deletePetrolDetails(${petroldetail.Id})">Delete</button>
-                <button type="button" class="btn btn-primary" data-toggle="modal"
-                    data-target="#editpetroldetailsModal" onclick="setEditModal(${petroldetail.Id})">
-                    Edit
-                </button>
-            </div>
+  }
+  location.reload();
+}
+  //Load Petrol details in view page
+function LoadPetrolDetails(){
+const xhr = new XMLHttpRequest();
+let url = " https://petrolappapi.herokuapp.com/petroldetails";
+xhr.open("GET",url,false);
+xhr.send();
+const petroldetails = JSON.parse(xhr.responseText);
+for(let petroldetail of petroldetails){
+  const petrolview = `
+    <div class="col-4">
+    <div class="card text-center text-white bg-dark border-dark mb-3">
+        <div class="card-body">
+            <h5 class="card-title">${petroldetail.Date}</h5>
+            <h6 class="card-subtitle mb-2">${petroldetail.Id}</h6>
+            <h6 class="card-subtitle mb-2">${petroldetail.Mileage}</h6>
+            <div>Amount: ${petroldetail.Amount}</div>
+            <div>Price Per Liter: ${petroldetail.PpL}</div>
+            <div>Capacity: ${petroldetail.Capacity}</div>
+            <div>Petrol Station: ${petroldetail.PetrolStation}</div>
+            <div>License Plate: ${petroldetail.LicensePlate}</div>
+            <div>Additional Info: ${petroldetail.AdditionalInfo}</div>
+            <hr>
+            <button type="button" class="btn btn-danger" onclick="deletePetrolDetails(${petroldetail.Id})">Delete</button>
+            <button type="button" class="btn btn-primary" data-toggle="modal"
+                data-target="#editpetroldetailsModal" onclick="setEditModal(${petroldetail.Id})">
+                Edit
+            </button>
         </div>
-      </div>
-      `
-      document.getElementById('petroldetails').innerHTML = document.getElementById('petroldetails').innerHTML + petrolview;
-    }
+    </div>
+  </div>
+  `
+  document.getElementById('petroldetails').innerHTML = document.getElementById('petroldetails').innerHTML + petrolview;
+}
 }
 LoadPetrolDetails();
